06718506d9ccf6f0bdb4ccce55bc64304e356948
Vignesh Edithal <vigneshedithal11031997v@gmai.com>
2022-12-01 08:19:32 -0500

    Add DCGAN model

---
+++ src/models/dcgan_00_07_01_12/model.py
@@ -0,0 +1,108 @@
+import os
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.nn as nn
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from tqdm import tqdm
+
+from data import BraTSDataset
+
+from IPython.display import HTML
+
+import numpy as np
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from skimage.metrics import structural_similarity as ssim
+
+dataset_root = "dataset"
+t1_train_data = "data/MICCAI_BraTS2020/train/t1"
+image_size = 64
+batch_size = 128
+num_workers = 16
+device = torch.device("cuda:0")
+
+# Generator model configuration
+latent_size = 128
+feature_map_size = image_size//2
+
+# Learning rate
+lr = 0.0002
+# beta 1 for Adam
+beta1 = 0.5
+
+# Number of training epochs
+num_epochs = 500
+
+def weight_init(m):
+    """Custom weight initialization called on netG and netD"""
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.normal_(m.bias.data, 0)
+
+# Generator code
+class Generator(nn.Module):
+    def __init__(self) -> None:
+        super(Generator, self).__init__()
+        self.main = nn.Sequential(
+            # input: latent_size x 1 x 1
+            nn.ConvTranspose2d(latent_size, feature_map_size * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(feature_map_size * 8),
+            nn.ReLU(True),
+            # input: feature_map_size*16 x image_size/16 x image_size/16
+            nn.ConvTranspose2d(feature_map_size * 8, feature_map_size * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_map_size * 4),
+            nn.ReLU(True),
+            # input: feature_map_size*8 x image_size/8 x image_size/8
+            nn.ConvTranspose2d(feature_map_size * 4, feature_map_size * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_map_size * 2),
+            nn.ReLU(True),
+            # input: feature_map_size*4 x image_size/4 x image_size/4
+            nn.ConvTranspose2d(feature_map_size*2, feature_map_size, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_map_size),
+            nn.ReLU(True),
+            # input: feature_map_size*2 x image_size/2 x image_size/2
+            nn.ConvTranspose2d(feature_map_size, 1, 4, 2, 1, bias=False),
+            nn.Tanh(),
+            # output: 1 x image_size x image_size
+        )
+    
+    def forward(self, input):
+        return self.main(input)
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+            # input: 1 x image_size x image_size
+            nn.Conv2d(1, feature_map_size, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_map_size),
+            nn.LeakyReLU(0.2, True),
+            # input: feature_map_size x image_size/2 x image_size/2
+            nn.Conv2d(feature_map_size, feature_map_size * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_map_size * 2),
+            nn.LeakyReLU(0.2, True),
+            # input: feature_map_size*2 x image_size/4 x image_size/4
+            nn.Conv2d(feature_map_size * 2, feature_map_size * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_map_size * 4),
+            nn.LeakyReLU(0.2, True),
+            # input: feature_map_size*4 x image_size/8 x image_size/8
+            nn.Conv2d(feature_map_size * 4, feature_map_size * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(feature_map_size * 8),
+            nn.LeakyReLU(0.2, True),
+            # input: feature_map_size*8 x image_size/16 x image_size/16
+            nn.Conv2d(feature_map_size * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+            # output: 1 x 1 x 1
+        )
+    
+    def forward(self, input):
+        return self.main(input)
